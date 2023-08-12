1c6dee9c634a449e68e1a76377d77e1b31d46ae5
Paul Mueller <pmueller@mrtknecht3.mrt.uni-karlsruhe.de>
2023-08-12 17:10:46 +0200

    Test Code for Llama 2 Open Source

--- llama_test.py
+++ llama_test.py
@@ -1,5 +1,23 @@
-import streamlit as st
+### Testing code from: https://huggingface.co/upstage/Llama-2-70b-instruct-v2 ###
 
-st.title("Llama Test")
+import torch
+from transformers import AutoModelForCausalLM, AutoTokenizer, TextStreamer
 
-st.write("This is a test of the llama.")
+tokenizer = AutoTokenizer.from_pretrained("upstage/Llama-2-70b-instruct-v2")
+model = AutoModelForCausalLM.from_pretrained(
+    "upstage/Llama-2-70b-instruct-v2",
+    device_map="auto",
+    torch_dtype=torch.float16,
+    load_in_8bit=True,
+    rope_scaling={"type": "dynamic", "factor": 2} # allows handling of longer inputs
+)
+
+prompt = "### User:\nThomas is healthy, but he has to go to the hospital. What could be the reasons?\n\n### Assistant:\n"
+inputs = tokenizer(prompt, return_tensors="pt").to(model.device)
+del inputs["token_type_ids"]
+streamer = TextStreamer(tokenizer, skip_prompt=True, skip_special_tokens=True)
+
+output = model.generate(**inputs, streamer=streamer, use_cache=True, max_new_tokens=float('inf'))
+output_text = tokenizer.decode(output[0], skip_special_tokens=True)
+
+# Apparently we only have 11,55 GB of VRAM on this machine which makes it impossible to execute the model
