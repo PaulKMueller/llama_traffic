7cdd1be469efe3047965bbd1afa867426b1e5fa1
Paul Mueller <pmueller@mrtknecht4.mrt.uni-karlsruhe.de>
2023-08-24 17:57:33 +0200

    Llama and BERT embeddings

---
+++ embedding_test.py
@@ -0,0 +1,19 @@
+import torch
+from transformers import BertTokenizer, BertModel
+
+# Load BERT tokenizer and model
+tokenizer = BertTokenizer.from_pretrained('bert-base-uncased')
+model = BertModel.from_pretrained('bert-base-uncased')
+
+# Encode the input text
+input_text = "Hello, BE!"
+encoded_input = tokenizer(input_text, return_tensors='pt')
+
+# Get BERT embeddings
+with torch.no_grad():
+    output = model(**encoded_input)
+    # `output` is a tuple, with the first element being the sequence of hidden-states at the output of the last layer
+    # It has shape (batch_size, sequence_length, hidden_size), so we pick the first item in the sequence (for [CLS]) as our embedding for the whole sentence
+    embedding = output[0][0][0]
+
+print(embedding)
--- llama_test.py
+++ llama_test.py
@@ -5,13 +5,14 @@ model = LlamaForCausalLM.from_pretrained('meta-llama/Llama-2-13b-chat-hf')
 tokenizer = LlamaTokenizer.from_pretrained('meta-llama/Llama-2-13b-chat-hf')
 
 # Text to generate from
-input_text = "Produce a set of Vectors in the following format: \n V: 1, 3, 6, 2, 5"
+input_text = "Produce a set of Vectors in the following format: \n V: 1, 3, 6, 2, 5 \n Generated Vectors:"
 input_ids = tokenizer.encode(input_text, return_tensors="pt")
+print(input_ids)
 
 # Generate text
 # The `generate` method returns token ids which we can decode back to text
-output_ids = model.generate(input_ids, max_length=50)  # you can customize max_length
-output_text = tokenizer.decode(output_ids[0], skip_special_tokens=True)
+# output_ids = model.generate(input_ids, max_length=100)  # you can customize max_length
+# output_text = tokenizer.decode(output_ids[0], skip_special_tokens=True)
 
-print(output_text)
+# print(output_text)
 
